cfdc5498a4ba124e1af897d4bd807869e1d67b4a
mohamed00Sayed <sayedm22529@gmail.com>
2021-10-17 19:35:46 +0200

    test:adding tests

---
+++ src/test/java/com/sayed/placeholders/test/EnvironmentConfig.java
@@ -0,0 +1,25 @@
+package com.sayed.placeholders.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import com.sayed.placeholders.BlankDisc;
+
+@Configuration
+@PropertySource("classpath:/com/soundsystem/app.properties")
+public class EnvironmentConfig {
+
+  @Autowired
+  Environment env;
+  
+  @Bean
+  public BlankDisc blankDisc() {
+    return new BlankDisc(
+        env.getProperty("disc.title"),
+        env.getProperty("disc.artist"));
+  }
+  
+}
---
+++ src/test/java/com/sayed/placeholders/test/EnvironmentConfigWithDefaults.java
@@ -0,0 +1,23 @@
+package com.sayed.placeholders.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import com.sayed.placeholders.BlankDisc;
+
+@Configuration
+public class EnvironmentConfigWithDefaults {
+
+  @Autowired
+  Environment env;
+  
+  @Bean
+  public BlankDisc blankDisc() {
+    return new BlankDisc(
+        env.getProperty("disc.title", "Rattle and Hum"),
+        env.getProperty("disc.artist", "U2"));
+  }
+  
+}
